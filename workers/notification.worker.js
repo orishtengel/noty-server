c54bb519394a6e8c52f23cac46e2505b6f148777
Gal Shtengel <gals@skywatch.ai>
2022-03-11 20:52:46 +0200

    taking the whole week in advance

--- workers/notification.worker.js
+++ workers/notification.worker.js
@@ -15,42 +15,54 @@ class NotificationWorker {
 
     crawl() {
         const crawlers = createCrawlers()
-        crawlers.map(async crawler => {
-            const url = crawler.url
-            const availableDates = await crawler.getAvailableDates()
-            const subscriptions = await getSubscriptionsById(crawler.id)
-            console.log(availableDates, subscriptions)
-            for (const id in subscriptions) {
 
-                const momentToday = moment(dayjs().format('MM/DD/YYYY'), 'MM/DD/YYYY')
-                const momentSub = moment(subscriptions[id].endTime.split(' ')[0], 'MM/DD/YYYY')
-                if(momentSub.diff(momentToday) < 0) {
-                    // old subscription to delete
-                    await deleteSubscribe(crawler.id, id)
-                    continue
-                }
-
-                const datesAvailable = []
-                availableDates.map(availableDate => {
-                    let availableDateJs = dayjs(availableDate.date)
-                    // comapring the month,day, year part of the dates
-                    if(moment(subscriptions[id].date, 'MM/DD/YYYY').diff(moment(availableDate.dayDate, 'MM/DD/YYYY'), 'days') == 0 ) {
-                        if(moment(`${availableDate.dayDate} ${availableDate.hour}`, 'MM/DD/YYYY HH:mm').isBetween(
-                            moment(subscriptions[id].startTime, 'MM/DD/YYYY HH:mm'),
-                            moment(subscriptions[id].endTime, 'MM/DD/YYYY HH:mm')
-                        )) {
-                            datesAvailable.push(availableDateJs)
-                        }
+        const Dates = [
+            dayjs().toDate(),
+            dayjs().add(1, 'day').toDate(),
+            dayjs().add(2, 'day').toDate(),
+            dayjs().add(3, 'day').toDate(),
+            dayjs().add(4, 'day').toDate(),
+            dayjs().add(5, 'day').toDate(),
+            dayjs().add(6, 'day').toDate(),
+            dayjs().add(7, 'day').toDate(),
+        ].map(askingDate => {
+            crawlers.map(async crawler => {
+                const url = crawler.url
+                const availableDates = await crawler.getAvailableDates(askingDate)
+                const subscriptions = await getSubscriptionsById(crawler.id)
+                console.log(availableDates, subscriptions)
+                for (const id in subscriptions) {
+    
+                    const momentToday = moment(dayjs().format('MM/DD/YYYY'), 'MM/DD/YYYY')
+                    const momentSub = moment(subscriptions[id].endTime.split(' ')[0], 'MM/DD/YYYY')
+                    if(momentSub.diff(momentToday) < 0) {
+                        // old subscription to delete
+                        await deleteSubscribe(crawler.id, id)
+                        continue
                     }
-                })
-
-                if(datesAvailable.length == 0)
-                    continue
-
-                let user = await getUser(subscriptions[id].email)
-                
-                notificationManager.sendNotification(crawler, subscriptions[id], user, datesAvailable)
-            }
+    
+                    const datesAvailable = []
+                    availableDates.map(availableDate => {
+                        let availableDateJs = dayjs(availableDate.date)
+                        // comapring the month,day, year part of the dates
+                        if(moment(subscriptions[id].date, 'MM/DD/YYYY').diff(moment(availableDate.dayDate, 'MM/DD/YYYY'), 'days') == 0 ) {
+                            if(moment(`${availableDate.dayDate} ${availableDate.hour}`, 'MM/DD/YYYY HH:mm').isBetween(
+                                moment(subscriptions[id].startTime, 'MM/DD/YYYY HH:mm'),
+                                moment(subscriptions[id].endTime, 'MM/DD/YYYY HH:mm')
+                            )) {
+                                datesAvailable.push(availableDateJs)
+                            }
+                        }
+                    })
+    
+                    if(datesAvailable.length == 0)
+                        continue
+    
+                    let user = await getUser(subscriptions[id].email)
+                    
+                    notificationManager.sendNotification(crawler, subscriptions[id], user, datesAvailable)
+                }
+            })
         })
     }
 }
@@ -60,7 +72,7 @@ module.exports = {
     start: async () => {
         const worker = new NotificationWorker()
         setInterval(() => {
-            // worker.crawl()
-        }, 6000)
+            worker.crawl()
+        }, 1000 * 60)
     }
 }
