b7100dba22e90f12bb46b006c692b1a18f142130
Gal Shtengel <gals@skywatch.ai>
2022-03-28 20:57:53 +0300

    river ridge

--- applications/crawlers/crawler.factory.js
+++ applications/crawlers/crawler.factory.js
@@ -2,6 +2,7 @@ const WestlakeCrawler = require("./crawler.westlake")
 const EzLinksCamariloSpringsCrawler = require("./ezlinks.camarilo.springs.crawler")
 const EzLinksDeBellCrawler = require("./ezlinks.debell.crawler")
 const EzLinksGolfCrawler = require("./ezlinks.gobles.crawler")
+const RiverRidgeCrawler = require("./river.ridge.crawler")
 
 module.exports = {
     createCrawlers : () => {
@@ -9,7 +10,8 @@ module.exports = {
             new EzLinksGolfCrawler(),
             new WestlakeCrawler(),
             new EzLinksCamariloSpringsCrawler(),
-            new EzLinksDeBellCrawler()
+            new EzLinksDeBellCrawler(),
+            new RiverRidgeCrawler()
         ]
     }
 }
