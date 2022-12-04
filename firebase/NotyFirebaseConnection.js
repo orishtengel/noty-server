b148255ef4ad0ec7b5b57931e60c51564fb0f76c
אורי שטנגל <wrystngl@wrys-MacBook-Pro.local>
2022-12-04 21:03:35 +0200

    delete firebase

--- firebase/NotyFirebaseConnection.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin')
-
-const serviceAccount = require('./ezlinks-1b7b7-firebase-adminsdk-a4u13-8316ed2199.json')
-
-admin.initializeApp({
-    credential : admin.credential.cert(serviceAccount)
-})
-
-module.exports = admin
